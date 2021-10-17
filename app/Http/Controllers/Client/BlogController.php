f58359ce1e39e2e69d9c01bb3f6d15586fa50401
Quangcoder2002 <vntth12@gmail.com>
2021-10-17 15:06:54 +0700

    up

---
+++ app/Http/Controllers/Client/BlogController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::where('status', Post::STATUS_PUBLIC)->orderBy('created_at', 'desc')->paginate(6);
+        return view('client.blog')->with([
+            'posts' => $posts
+        ]);
+    }
+    // public function category()
+    // {
+    //     $categories = Category::get();
+    //     $posts = Post::where('status', Post::STATUS_PUBLIC)->orderBy('created_at', 'desc')->paginate(6);
+    //     return view('client.categories-post')->with([
+    //         'posts' => $posts,
+    //         'categories' => $categories
+    //     ]);
+    // }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Post::find($id);
+        $tags = Tag::get();
+        return view('client.blog-single-post')->with([
+            'post' => $post,
+            'tags' => $tags
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
